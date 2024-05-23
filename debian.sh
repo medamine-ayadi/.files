69d3d88b38b5b96728f5b462621e98d548af05dd
medamine-ayadi <mohamedamine.ayadi@medtech.tn>
2024-05-23 20:25:11 +0100

    added a browser

--- debian.sh
+++ debian.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
+wget https://github.com/Alex313031/Mercury/releases/download/v.123.0.1/mercury-browser_123.0.1_SSE4.deb -P /tmp/
 PKGS=(
-	'neovim'
-	'git'
-	'sway'
-	'wofi'
-	'foot'
-	'i3status'
-	'mpv'
-	'fzf'
-	'bash-completion'
-	'pipewire'
-	'wireplumber'
-	'pipewire-audio'
-	'grim'
-	'swappy'
-	'slurp'
-	'brightnessctl'
-	'imv'
-	'stow'
+    'neovim'
+    'git'
+    'sway'
+    'wofi'
+    'foot'
+    'i3status'
+    'mpv'
+    'fzf'
+    'bash-completion'
+    'pipewire'
+    'wireplumber'
+    'pipewire-audio'
+    'grim'
+    'swappy'
+    'slurp'
+    'brightnessctl'
+    'imv'
+    'stow'
+    '/tmp/mercury-browser_123.0.1_SSE4.deb'
 )
 
 for PKG in "${PKGS[@]}"; do
@@ -29,11 +31,11 @@ done
 # == Stow all dirs ==
 # stow */
 CONFIGS=(
-	'foot'
-	'i3status'
-	'sway'
-	'wofi'
-	'nvim'
+    'foot'
+    'i3status'
+    'sway'
+    'wofi'
+    'nvim'
 )
 for CONFIG in "${CONFIGS[@]}"; do
     echo "STOWING: ${CONFIG}"
